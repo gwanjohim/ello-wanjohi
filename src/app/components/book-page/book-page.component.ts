1fe26c9876d01566b817ec67755c928d5fde2d23
gwanjohim <gwanjohim@gmail.com>
2022-06-01 20:24:22 +0300

    Capturing all words specified in the tokens

--- src/app/components/book-page/book-page.component.ts
+++ src/app/components/book-page/book-page.component.ts
@@ -2,71 +2,41 @@ import { JsonPipe } from '@angular/common';
 import { escapeRegExp } from '@angular/compiler/src/util';
 import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { PageSubstring } from 'src/app/page-sub-string';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-book-page',
   templateUrl: './book-page.component.html',
   styleUrls: ['./book-page.component.scss']
 })
-export class BookPageComponent implements OnInit, AfterViewInit, OnChanges, AfterContentInit, AfterContentChecked {
+export class BookPageComponent implements OnInit, AfterViewInit {
 
   @Input() PageText: string;
   @Input() PageTokens: PageSubstring[]
-  @ViewChild('pageContent') pageContent: ElementRef
 
-  constructor() { }
-  ngAfterContentChecked(): void {
-
-  }
-  ngAfterContentInit(): void {
-
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-
-    // for (const propName in changes) {
-    //   const chng = changes['PageTokens'];
-    //   const cur = JSON.stringify(chng.currentValue);
-    //   const prev = JSON.stringify(chng.previousValue);
-
-    //   if (cur !== prev) {
-    //     let result = JSON.parse(cur)
-    //     this.PageTokens = result
-    //   }
-    // }
-
-
-
-
+  constructor(@Inject(DOCUMENT) public document: Document) {
   }
   ngAfterViewInit(): void {
 
-    let documentNode = document.createElement("span")
+    var textInput = this.PageText;
 
-    this.pageContent.nativeElement.innerHTML = this.PageText
+    let resultText = this.PageTokens.reduce((previous, token) => {
 
-    this.PageTokens.forEach(tokn => {
+      let word = textInput.substring(token.startIndex, token.endIndex)
+      var replacement = '<span>' + word + '</span>'
+      let current = textInput.substring(token.startIndex, token.endIndex).replace(word, replacement)
+      return previous + current
+    }, '')
 
-      setTimeout(() => {
+    var docu = document.getElementById('pageContent')
+    docu!.outerText = resultText
 
-        let word = this.PageText.substring(tokn.startIndex, tokn.endIndex)
-        let patternToReplace = `\b${word}\b`;
-        let replacementWord = word;
-        documentNode.innerHTML = replacementWord
-        documentNode.className = "bold";
-        let newContent = this.pageContent.nativeElement.innerHTML.replace(word, documentNode.outerHTML);
 
-        this.pageContent.nativeElement.innerHTML = newContent
-        // htmlString.replace(patternToReplace, replacement)
-      }, 1000);
-      //   setTimeout(() => {
-
-      //   }, 1000);
-      });
-
-    }
+  }
 
   ngOnInit(): void {
 
-    }
+  }
 
 }
