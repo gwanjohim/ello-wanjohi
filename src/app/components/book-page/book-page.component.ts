7d1fbbcf658293f32810a8b9e664a32684db394d
gwanjohim <gwanjohim@gmail.com>
2022-06-01 13:43:13 +0300

    partial implementation,  hughighting words

--- src/app/components/book-page/book-page.component.ts
+++ src/app/components/book-page/book-page.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { escapeRegExp } from '@angular/compiler/src/util';
+import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { PageSubstring } from 'src/app/page-sub-string';
 
 @Component({
@@ -6,26 +8,65 @@ import { PageSubstring } from 'src/app/page-sub-string';
   templateUrl: './book-page.component.html',
   styleUrls: ['./book-page.component.scss']
 })
-export class BookPageComponent implements OnInit, AfterViewInit {
+export class BookPageComponent implements OnInit, AfterViewInit, OnChanges, AfterContentInit, AfterContentChecked {
 
   @Input() PageText: string;
   @Input() PageTokens: PageSubstring[]
-
   @ViewChild('pageContent') pageContent: ElementRef
+
   constructor() { }
-  ngAfterViewInit(): void {
-    let paragraphElement = document.createElement('p')
-    
-    let textNode = document.createTextNode(this.PageText)
-    this.PageTokens.forEach(tokn => {
-      let word = textNode.data.substring(tokn.startIndex, tokn.endIndex)
-      textNode.data.replace(word, 'x')
-      // console.error(`actual ${word}: tokenized ${tokn.subString}`);
-    })
-    this.pageContent.nativeElement.appendChild(textNode)
+  ngAfterContentChecked(): void {
+
   }
+  ngAfterContentInit(): void {
+
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+
+    // for (const propName in changes) {
+    //   const chng = changes['PageTokens'];
+    //   const cur = JSON.stringify(chng.currentValue);
+    //   const prev = JSON.stringify(chng.previousValue);
+
+    //   if (cur !== prev) {
+    //     let result = JSON.parse(cur)
+    //     this.PageTokens = result
+    //   }
+    // }
+
+
+
+
+  }
+  ngAfterViewInit(): void {
+
+    let documentNode = document.createElement("span")
+
+    this.pageContent.nativeElement.innerHTML = this.PageText
+
+    this.PageTokens.forEach(tokn => {
+
+      setTimeout(() => {
+
+        let word = this.PageText.substring(tokn.startIndex, tokn.endIndex)
+        let patternToReplace = `\b${word}\b`;
+        let replacementWord = word;
+        documentNode.innerHTML = replacementWord
+        documentNode.className = "bold";
+        let newContent = this.pageContent.nativeElement.innerHTML.replace(word, documentNode.outerHTML);
+
+        this.pageContent.nativeElement.innerHTML = newContent
+        // htmlString.replace(patternToReplace, replacement)
+      }, 1000);
+      //   setTimeout(() => {
+
+      //   }, 1000);
+      });
+
+    }
 
   ngOnInit(): void {
-  }
+
+    }
 
 }
