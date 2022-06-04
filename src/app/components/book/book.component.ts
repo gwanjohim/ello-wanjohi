80637b8ee88dd62c709ffcaf2ef9eb7860bd71e1
gwanjohim <gwanjohim@gmail.com>
2022-06-05 00:26:21 +0300

    navigating back and forth .. Linear algebra. Note, subtration comes in last. It might use a condition.

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -16,14 +16,32 @@ export class BookComponent implements OnInit {
   title = 'ello-wanjohi';
 
   pages: Array<Page> = []
-  currentPage = 0;
+  // currentPage = 0;
 
   offset = 0;
-
   pageCount = 2;
+  currentlyShownPages: Page[] = []
 
   constructor(private apollo: Apollo) {
   }
+
+  createPageItems(pagesPerView: number) {
+    // var items: number[] = [];
+    // for(var i = 1; i <= number; i++){
+    //   items.push(i);
+    // }
+    // return items;
+    return new Array<number>(pagesPerView)
+  }
+
+  // createRange(number: number) {
+  //   // var items: number[] = [];
+  //   // for(var i = 1; i <= number; i++){
+  //   //   items.push(i);
+  //   // }
+  //   // return items;
+  //   return new Array(number);
+  // }
 
   ngOnInit() {
     this.apollo
@@ -48,68 +66,74 @@ export class BookComponent implements OnInit {
         this.error = result.error
         let book = result?.data?.book
         this.book = book
+        this.initializeView()
 
-        let pagesCount = this.book!.pages.length;
+        // let pagesCount = this.book!.pages.length;
 
-        if (this.currentPage != pagesCount) {
-          let firstPage = this.book!.pages[this.currentPage]
-          let secondPage = this.book!.pages[this.currentPage + 1]
-          this.pages = [firstPage, secondPage]
-          this.currentPage = this.currentPage + 2
-        }
+        // if (this.currentPage != pagesCount) {
+        //   let firstPage = this.book!.pages[this.currentPage]
+        //   let secondPage = this.book!.pages[this.currentPage + 1]
+        //   this.pages = [firstPage, secondPage]
+        //   this.currentPage = this.currentPage + 2
+        // }
 
       });
   }
 
+  initializeView() {
+    this.currentlyShownPages = this.book!.pages.slice(this.offset, this.pageCount)
+  }
   goToNextDoublePage() {
 
+    this.offset = this.offset + this.pageCount;
 
-    return this.pages.slice(((this.offset) - (this.pageCount)), (this.pageCount));
+    this.currentlyShownPages = this.book!.pages.slice(this.offset, (this.offset + this.pageCount));
 
-    let _currentPage = this.currentPage;
 
-    let pagesCount = this.book!.pages.length;
-    if (_currentPage == 0) {
-      let firstPage = this.book!.pages[0]
-      let secondPage = this.book!.pages[1]
-      this.pages = []
-      this.pages = [firstPage, secondPage]
-
-      this.currentPage = this.currentPage + 2;
-    }
-    if (this.currentPage > 0 && this.currentPage < pagesCount) {
-
-      let firstPage = this.book!.pages[_currentPage + 1]
-      let secondPage = this.book!.pages[_currentPage + 2]
-      this.pages = []
-      this.pages = [firstPage, secondPage]
-      this.currentPage = _currentPage + 2;
-    }
-    if (this.currentPage === pagesCount) {
-      return;
-      // let firstPage = this.book!.pages[_currentPage]
-      // let pages = [firstPage]
-      // this.pages.splice(0, 1, ...pages)
-    }
-  }
-
-  goToPreviousDoublePage() {
-
-    return this.pages.slice(((this.offset) - (this.pageCount)), (this.pageCount));
 
     // let _currentPage = this.currentPage;
 
     // let pagesCount = this.book!.pages.length;
+    // if (_currentPage == 0) {
+    //   let firstPage = this.book!.pages[0]
+    //   let secondPage = this.book!.pages[1]
+    //   this.pages = []
+    //   this.pages = [firstPage, secondPage]
 
-    // if (this.currentPage <= 0) {
+    //   this.currentPage = this.currentPage + 2;
+    // }
+    // if (this.currentPage > 0 && this.currentPage < pagesCount) {
+
+    //   let firstPage = this.book!.pages[_currentPage + 1]
+    //   let secondPage = this.book!.pages[_currentPage + 2]
+    //   this.pages = []
+    //   this.pages = [firstPage, secondPage]
+    //   this.currentPage = _currentPage + 2;
+    // }
+    // if (this.currentPage === pagesCount) {
+    //   return;
     //   // let firstPage = this.book!.pages[_currentPage]
     //   // let pages = [firstPage]
     //   // this.pages.splice(0, 1, ...pages)
-    //   return;
     // }
+  }
 
-    // if (_currentPage <= pagesCount) {
-    //   if (_currentPage === 2) {
+  goToPreviousDoublePage() {
+
+
+
+    this.currentlyShownPages = this.book!.pages.slice((this.offset - this.pageCount), this.offset);
+
+    this.offset = this.offset - this.pageCount;
+
+
+
+    // let _currentPage = this.currentPage;
+
+    // let pagesCount http://transcendentwholeinnerlaugh.neverssl.com/onlinetPage <= 0) {
+    //   // let firstPage = this.book!.pages[_currentPage]
+    //   // let pages = [firstPage]
+    //   // this.pageshttp://transcendentwholeinnerlaugh.neverssl.com/onlineage === 2) {
     //     return;
     //   }
     //   _currentPage = _currentPage - 2;
